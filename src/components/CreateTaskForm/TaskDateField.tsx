a94d96ef63caf4e5793cdd7bffebff25dcdca483
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-06-02 22:34:13 +0530

    add interface and proptype for date field

--- src/components/CreateTaskForm/TaskDateField.tsx
+++ src/components/CreateTaskForm/TaskDateField.tsx
@@ -1,21 +1,31 @@
-import React, { FC, ReactElement, useState } from 'react';
+import React, { FC, ReactElement } from 'react';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TextField } from '@mui/material';
+import { IDateField } from './interfaces/IDateField';
+import PropTypes from 'prop-types';
 
-const TaskDateField: FC = (): ReactElement => {
-  const [date, setDate] = useState<Date | null>(null);
+const TaskDateField: FC<IDateField> = (props): ReactElement => {
+  const {
+    value = new Date(),
+    onChange = date => {
+      console.log(date);
+    },
+    disabled = false,
+  } = props;
+
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <DesktopDatePicker
           label='Task Date'
           format='dd/MM/yyyy'
-          value={date}
-          onChange={newValue => setDate(newValue)}
+          value={value}
+          disabled={disabled}
+          onChange={onChange}
           // @ts-ignore
-          renderInput={params => <TextField {...params} />} 
+          renderInput={params => <TextField {...params} />}
         />
       </LocalizationProvider>
     </>
@@ -23,3 +33,9 @@ const TaskDateField: FC = (): ReactElement => {
 };
 
 export default TaskDateField;
+
+TaskDateField.propTypes = {
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  value: PropTypes.instanceOf(Date),
+};
