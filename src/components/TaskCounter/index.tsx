9ab414dabede773826ac34032a881014f3b1861f
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-06-04 18:33:10 +0530

    add task-area and tak-counter layout and styles

---
+++ src/components/TaskCounter/index.tsx
@@ -0,0 +1,40 @@
+import { Box, Typography, Avatar } from '@mui/material';
+import React, { FC, ReactElement } from 'react';
+
+const TaskCounter: FC = (): ReactElement => {
+  return (
+    <>
+      <Box
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Avatar
+          sx={{
+            backgroundColor: 'transparent',
+            border: '5px solid',
+            width: '96px',
+            height: '96px',
+            marginBottom: '16px',
+            borderColor: 'warning.light',
+          }}
+        >
+          <Typography color='#ffffff' variant='h4'>
+            10
+          </Typography>
+        </Avatar>
+        <Typography
+          color='#ffffff'
+          fontWeight='bold'
+          fontSize='20px'
+          variant='h5'
+        >
+          Subtitle
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+export default TaskCounter;
