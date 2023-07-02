92669669e0bb521e6be7c7cc7674c4a1fde8bd7b
Ayush <ayushagarwal.dev@gmail.com>
2023-07-02 22:09:31 +0530

    add global fetch API resusable function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, { FC, ReactElement } from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { customTheme } from './theme/customTheme';
-import { Dashboard } from './pages/Dashboard';
+import React, { FC, ReactElement } from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { customTheme } from "./theme/customTheme";
+import { Dashboard } from "./pages/Dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
