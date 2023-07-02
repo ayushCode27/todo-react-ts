32fb3f5b4eedad9fa790027cdf6e954f5943cd73
Ayush <ayushagarwal.dev@gmail.com>
2023-07-02 22:27:22 +0530

    add state and UI elements to CreateTaskForm component

--- src/components/CreateTaskForm/index.tsx
+++ src/components/CreateTaskForm/index.tsx
@@ -1,34 +1,60 @@
-import React, { FC, ReactElement } from 'react';
-import { Box, Typography, Stack } from '@mui/material';
-import TaskTitleField from './TaskTitleField';
-import TaskDescriptionField from './TaskDescriptionField';
-import TaskDateField from './TaskDateField';
-import TaskSelectField from './TaskSelectField';
-import { Status } from './enums/Status';
-import { Priority } from './enums/Priority';
+import React, { FC, ReactElement, useState } from "react";
+import {
+  Box,
+  Typography,
+  Stack,
+  LinearProgress,
+  Button,
+  Alert,
+  AlertTitle,
+} from "@mui/material";
+import TaskTitleField from "./TaskTitleField";
+import TaskDescriptionField from "./TaskDescriptionField";
+import TaskDateField from "./TaskDateField";
+import TaskSelectField from "./TaskSelectField";
+import { Status } from "./enums/Status";
+import { Priority } from "./enums/Priority";
 
 const CreateTaskForm: FC = (): ReactElement => {
+  const [title, setTitle] = useState<string | undefined>(undefined);
+  const [description, setDescription] = useState<string | undefined>(undefined);
+  const [date, setDate] = useState<Date | null>(new Date());
+  const [status, setStatus] = useState<string>(Status.todo);
+  const [priority, setPriority] = useState<string>(Priority.normal);
+
   return (
     <Box
-      display='flex'
-      flexDirection='column'
-      alignItems='flex-start'
-      width='100%'
+      display="flex"
+      flexDirection="column"
+      alignItems="flex-start"
+      width="100%"
       px={4}
       my={6}
     >
-      <Typography mb={2} component='h2' variant='h6'>
+      <Alert severity="success" sx={{ width: "100%", marginBottom: "16px" }}>
+        <AlertTitle>Success</AlertTitle>
+        The task have been created successfully
+      </Alert>
+      <Typography mb={2} component="h2" variant="h6">
         Create A Task
       </Typography>
 
-      <Stack sx={{ width: '100%' }} spacing={2}>
-        <TaskTitleField />
-        <TaskDescriptionField />
-        <TaskDateField value={new Date()} />
-        <Stack direction='row' sx={{ width: '100%' }} spacing={2}>
+      <Stack sx={{ width: "100%" }} spacing={2}>
+        <TaskTitleField
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <TaskDescriptionField
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <TaskDateField value={date} onChange={(date) => setDate(date)} />
+        <Stack direction="row" sx={{ width: "100%" }} spacing={2}>
           <TaskSelectField
-            label='Status'
-            name='status'
+            value={status}
+            onChange={(e) => setStatus(e.target.value as string)}
+            label="Status"
+            name="status"
             items={[
               { value: Status.todo, label: Status.todo.toUpperCase() },
               {
@@ -38,8 +64,10 @@ const CreateTaskForm: FC = (): ReactElement => {
             ]}
           />
           <TaskSelectField
-            label='Priority'
-            name='priority'
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as string)}
+            label="Priority"
+            name="priority"
             items={[
               { value: Priority.low, label: Priority.low },
               { value: Priority.high, label: Priority.high },
@@ -47,6 +75,10 @@ const CreateTaskForm: FC = (): ReactElement => {
             ]}
           />
         </Stack>
+        <LinearProgress />
+        <Button variant="contained" size="large" fullWidth>
+          Create a Task
+        </Button>
       </Stack>
     </Box>
   );
